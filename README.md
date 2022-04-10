11c0e5647c071fa90b1f5a40b44977a28c084739
Nic Carrig <cleamcarrig90@hotmail.com>
2022-04-10 05:09:35 -0700

    added randomize methods

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-# Password Generator Starter Code
-
 # User Story
 
 AS AN employee with access to sensitive data\
 I WANT to randomly generate a password that meets certain criteria\
-SO THAT I can create a strong password that provides greater security\
+SO THAT I can create a strong password that provides greater security
 
 # Acceptance Criteria
 
@@ -22,4 +20,4 @@ THEN my input should be validated and at least one character type should be sele
 WHEN all prompts are answered\
 THEN a password is generated that matches the selected criteria\
 WHEN the password is generated\
-THEN the password is either displayed in an alert or written to the page\
+THEN the password is either displayed in an alert or written to the page
--- assets/code/script.js
+++ assets/code/script.js
@@ -1,5 +1,80 @@
 // Assignment code here
+function generatePassword(){
+  var password = "";
+  var length = getLength();
+  var availableType = [];
+  var input = false;
 
+  //loop to make sure user inputs at least one type for password
+  while(availableType.length === 0){
+    //prompt for each input and add corresponding number to array
+    input = window.confirm("Would you like to use Uppercase?");
+
+    if(availableType.length === 0){
+      window.alert("Please choose at least one type to generate a password");
+    }
+  }
+
+  for (var i =0; i < length; i++){
+    //pick random number from type array and put it into switch statement to call function, then add it to the password string
+    var type = availableType[Math.floor(Math.random() * availableType.length)];
+    switch(type){
+      case 1:
+        password += getRandomUpper();
+        break;
+      case 2:
+        password += getRandomLower();
+        break;
+      case 3:
+        password += getRandomNumber();
+        break;
+      case 4:
+        password += getRandomSpecial();
+        break;
+      default:
+        console.log("Problem in the switch statment");
+    }
+
+  }
+
+  return password;
+};
+function getLength(){
+  //prompt for length and loop until a valid input is given
+  var passLength = window.prompt("How long do you want your password to be (between 8 and 128 characters)");
+  passLength = parseInt(passLength);
+  if (passLength >= 8 && passLength <= 128){
+    return passLength;
+  }
+  else{
+    window.alert("Invalid input. Please choose a number between 8 and 128");
+    getLength();
+  }
+}
+function getRandomUpper(){
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var index = Math.floor(Math.random() * upper.length);
+
+  return upper.charAt(index);
+}
+function getRandomLower(){
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var index = Math.floor(Math.random() * lower.length);
+
+  return lower.charAt(index);
+}
+function getRandomNumber(){
+  var num = "1234567890";
+  var index = Math.floor(Math.random() * num.length);
+
+  return num.charAt(index);
+}
+function getRandomSpecial(){
+  var special = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~";
+  var index = Math.floor(Math.random() * special.length);
+
+  return special.charAt(index);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
